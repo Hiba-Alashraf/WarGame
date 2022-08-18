a259e0542da1d03483546b757c1980e0888c77bd
Hiba-Alashraf <alashrab@sheridancollege.ca>
2022-08-18 19:32:58 -0400

    Game Project done

---
+++ src/ca/sheridancollege/project/Rank.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author Hiba Alashraf
+ */
+public enum Rank {
+    ACE, KING, QUEEN, JACK, 
+    TEN, NINE, EIGHT, SEVEN, SIX,
+    FIVE, FOUR, THREE, TWO
+
+}
