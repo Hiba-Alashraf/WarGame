f3e50f38715a2a80284255e55ca4a2650726abf9
Hiba-Alashraf <alashrab@sheridancollege.ca>
2022-08-18 19:31:16 -0400

    Game Project done

---
+++ src/ca/sheridancollege/project/WarCard.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author Hiba Alashraf
+ */
+public class WarCard extends Card {
+    private Suit suit;
+    private Rank rank;
+
+    public WarCard(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public void setSuit(Suit suit) {
+        this.suit = suit;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public void setRank(Rank rank) {
+        this.rank = rank;
+    }
+
+    @Override
+    public String toString() {
+        return "WarCard [suit=" + suit + ", rank=" + rank + "]";
+    }
+}
